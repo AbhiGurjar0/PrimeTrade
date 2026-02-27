bb9d1f8699c75ce269423519958869f8483cc1f5
Abhi Gurjar <abhishekbainsla190@gmail.com>
2026-02-27 10:04:10 +0530

    redis isssue fixed

--- backend/services/taskService.js
+++ backend/services/taskService.js
@@ -1,79 +1,76 @@
 // services/taskService.js
 
 const Task = require("../models/Task");
-const redisClient = require("../config/redis");
+const redis = require("../config/redis");
 
 // CREATE TASK
 exports.createTaskService = async (data, userId) => {
+
   const task = await Task.create({
     ...data,
     user: userId,
   });
 
   // Clear cache
-  if (redisClient) {
-    await redisClient.del(`tasks:${userId}`);
-  }
+  await redis.del(`tasks:${userId}`);
 
   return task;
 };
 
 // GET ALL TASKS (with caching)
 exports.getTasksService = async (userId) => {
+
   const cacheKey = `tasks:${userId}`;
 
-  if (redisClient) {
-    const cached = await redisClient.get(cacheKey);
+  // 1️⃣ Try Redis first
+  const cachedTasks = await redis.get(cacheKey);
 
-    if (cached) {
-      return JSON.parse(cached);
-    }
+  if (cachedTasks) {
+    console.log("Serving from Redis cache");
+    return cachedTasks;
   }
 
+  // 2️⃣ Fetch from DB
   const tasks = await Task.find({ user: userId });
 
-  if (redisClient) {
-    await redisClient.set(cacheKey, JSON.stringify(tasks), {
-      EX: 60, // cache for 60 seconds
-    });
-  }
+  // 3️⃣ Store in Redis (60 sec)
+  await redis.set(cacheKey, tasks, { ex: 60 });
 
   return tasks;
 };
 
 // GET SINGLE TASK
 exports.getTaskService = async (taskId, userId) => {
-  const task = await Task.findOne({
+
+  return await Task.findOne({
     _id: taskId,
     user: userId,
   });
-
-  return task;
 };
 
 // UPDATE TASK
 exports.updateTaskService = async (taskId, data, userId) => {
+
   const task = await Task.findOneAndUpdate(
     { _id: taskId, user: userId },
     data,
-    { returnDocument: "after" },
+    { returnDocument: "after" }
   );
 
-  if (redisClient) {
-    await redisClient.del(`tasks:${userId}`);
-  }
+  // Clear cache
+  await redis.del(`tasks:${userId}`);
 
   return task;
 };
 
 // DELETE TASK
 exports.deleteTaskService = async (taskId, userId) => {
+
   await Task.findOneAndDelete({
     _id: taskId,
     user: userId,
   });
 
-  if (redisClient) {
-    await redisClient.del(`tasks:${userId}`);
-  }
+  // Clear cache
+  await redis.del(`tasks:${userId}`);
 };
